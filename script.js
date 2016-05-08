a9f56aec5c62cd07dae88c874373612f8dae2084
eiroa <eiroa@gmail.com>
2016-05-08 18:05:32 -0300

    otra funcion para desa1

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
  function test(){console.log('important script')}; 
 
-function newFunction(){console.log('nueva funcion')};
+function newFunction(){console.log('nueva funcion');
+console.log('que funcion copada');}
+
+function newFunction2(parameter){console.log('nueva funcion con parametro '+parameter);}
